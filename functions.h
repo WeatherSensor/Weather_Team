315f0a95bef6564cb5286123d7ef41463e5aeb45
Ani <tamaryanani950@gmail.com>
2023-08-24 14:11:32 +0100

    Added function that turns leds on/off related to temperature

--- functions.h
+++ functions.h
@@ -10,7 +10,7 @@
 #include <regex>
 #include <functional>
 #include <string>
-
+#include <pigpio.h>
 bool keepRunning = true;
 
 // Function to validate MAC address using regular expression
@@ -18,24 +18,6 @@ inline bool isValidMacAddress(const std::string &mac) {
     // Regular expression pattern for a valid MAC address
     std::regex macPattern("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$");
     return std::regex_match(mac, macPattern);
-}
-
-// Function to retrieve and print data
-inline void retrieveAndPrintData(const std::string &mac_address, int max_temp, int min_temp) {
-    std::string data = get_data(mac_address);
-    if (data == "F") {
-        std::cerr << "Trying To Connect To The Sensor..." << std::endl;
-        std::cout << std::endl;
-        return;
-    }
-
-    SensorData values(data);
-
-    std::cout << "Value: " << data;
-    std::cout << "Voltage: " << values.getVoltage() << "mV" << std::endl;
-    std::cout << "Humidity: " << values.getHumidity() << "%" << std::endl;
-    std::cout << "Temperature: " << values.getTemperature() << " degrees Celsius" << std::endl;
-    std::cout << std::endl;
 }
 
 // Interval function that calls a given function at specified intervals
--- main.cpp
+++ main.cpp
@@ -1,24 +1,63 @@
+#include <pigpio.h>
 #include <iostream>
 #include <csignal>
 #include "functions.h"
 
+
 int main(int argc, char *argv[]) {
-    if (argc < 5) {
-        std::cerr << "Usage: " << argv[0] << " -m \"mac_address\" <max_temp> <min_temp>" << std::endl;
+    if (argc < 6) {
+        std::cerr << "Usage: " << argv[0] << " -m <mac_address> <intervalInSeconds> <max_temp> <min_temp>" << std::endl;
         return -1;
     }
 
     std::string mac_address = argv[2];
-    int max_temp_size = std::stoi(argv[3]);
-    int min_temp_size = std::stoi(argv[4]);
-    int intervalInSeconds = 20; // Set the interval value as needed
+    int max_temp = std::stoi(argv[3]);
+    int min_temp = std::stoi(argv[4]);
+    int intervalInSeconds = std::stoi(argv[5]);
+    signal(SIGINT, SIG_IGN); // Ignore the SIGTSTP signal
 
-    //signal(SIGINT, SIG_IGN); // Ignore the SIGINT signal
-    signal(SIGTSTP, SIG_IGN); // Ignore the SIGTSTP signal
+    if (gpioInitialise() < 0) {
+        std::cerr << "Failed to initialize pigpio" << std::endl;
+        return -1;
+    }
 
     setInterval([&](const std::string& mac_address, int max_temp, int min_temp) {
-        retrieveAndPrintData(mac_address, max_temp, min_temp);
-    }, mac_address, intervalInSeconds, max_temp_size, min_temp_size);
+        std::string data = get_data(mac_address);
+        if (data == "F") {
+            std::cerr << "Trying To Connect To The Sensor..." << std::endl;
+            std::cout << std::endl;
+            return;
+        }
+
+        SensorData values(data);
+
+        std::cout << "Value: " << data;
+        std::cout << "Voltage: " << values.getVoltage() << "mV" << std::endl;
+        std::cout << "Humidity: " << values.getHumidity() << "%" << std::endl;
+        std::cout << "Temperature: " << values.getTemperature() << " degrees Celsius" << std::endl;
+        std::cout << std::endl;
+
+        if (max_temp > values.getTemperature() && values.getTemperature() > min_temp) {
+            gpioWrite(17, 0);
+            gpioWrite(18, 0);
+            gpioWrite(4, 1);
+        }
+
+        if (max_temp < values.getTemperature()) {
+            gpioWrite(4, 0);
+            gpioWrite(18, 0);
+            gpioWrite(17, 1);
+        }
+	
+        if (min_temp > values.getTemperature()) {
+            gpioWrite(4, 0);
+            gpioWrite(17, 0);
+            gpioWrite(18, 1);
+        }
+    }, mac_address, max_temp, min_temp, intervalInSeconds);
+
+    // Cleanup pigpio
+    gpioTerminate();
 
     return 0;
 }
