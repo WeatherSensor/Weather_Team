36d1c9ae1b8a7c47611e27f5d4e0d9581ee8f4f1
Ani <tamaryanani950@gmail.com>
2023-08-24 15:55:17 +0100

    added function to turn off all leds after pressing Ctrl Z, the code was optimized

--- functions.h
+++ functions.h
@@ -11,32 +11,98 @@
 #include <functional>
 #include <string>
 #include <pigpio.h>
+
 bool keepRunning = true;
 
+inline void handleSignal(int signum) {
+    if (signum == SIGTSTP) {
+        std::cout << "Ctrl+Z pressed. Cleaning up and terminating..." << std::endl;
+        gpioWrite(25, 0);
+        gpioWrite(17, 0);
+        gpioWrite(18, 0); 
+        gpioWrite(4, 0);
+        gpioTerminate();
+	exit(0);
+    }
+}
 
-void turnOffPins() {
-    gpioWrite(17, 0); // Turn off LED on GPIO 17
-    gpioWrite(18, 0); // Turn off LED on GPIO 18
-    gpioWrite(4, 0);  // Turn off LED on GPIO 4
+
+inline void getInfo(const std::string& mac_address, const float max_temp, const float min_temp){
+
+/*	if (gpioInitialise() < 0) {
+		std::cerr << "Failed to initialize pigpio" << std::endl;
+		return -1;
+	}
+*/
+	std::string data = get_data(mac_address);
+	if (data == "F") {
+		std::cerr << "Trying To Connect To The Sensor..." << std::endl;
+		std::cout << std::endl;
+		return;
+	}
+
+	SensorData values(data);
+
+	std::cout << "Value: " << data;
+	std::cout << "Voltage: " << values.getVoltage() << "mV" << std::endl;
+	std::cout << "Humidity: " << values.getHumidity() << "%" << std::endl;
+	std::cout << "Temperature: " << values.getTemperature() << " degrees Celsius" << std::endl;
+	std::cout << std::endl;
+
+	if (max_temp > values.getTemperature() && values.getTemperature() > min_temp) {
+		gpioWrite(25, 0);
+		gpioWrite(17, 0);
+		gpioWrite(18, 0);
+		gpioWrite(4, 1);
+	}
+
+	else if(max_temp< values.getTemperature()) {
+
+		gpioWrite(4, 0);
+		gpioWrite(18, 0);
+		gpioWrite(25, 1);
+		while(true){
+			gpioDelay(300000);
+			gpioWrite(17, 1);
+			gpioDelay(300000);
+			gpioWrite(17, 0);
+
+		}
+	}
+
+	else {
+
+		gpioWrite(25, 1);
+		gpioWrite(4, 0);
+		gpioWrite(17, 0);
+
+		while(true){
+			gpioDelay(300000);
+			gpioWrite(18, 1);
+			gpioDelay(300000);
+			gpioWrite(18, 0);
+
+		}
+	}
 }
 
 // Function to validate MAC address using regular expression
 inline bool isValidMacAddress(const std::string &mac) {
-    // Regular expression pattern for a valid MAC address
-    std::regex macPattern("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$");
-    return std::regex_match(mac, macPattern);
+	// Regular expression pattern for a valid MAC address
+	std::regex macPattern("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$");
+	return std::regex_match(mac, macPattern);
 }
 
 // Interval function that calls a given function at specified intervals
 template <typename Callable>
 void setInterval(Callable func, const std::string& mac_address, int intervalInSeconds, int max_temp, int min_temp) {
-    while (keepRunning) {
-        // Call the provided function with the given arguments
-        func(mac_address, max_temp, min_temp);
+	while (keepRunning) {
+		// Call the provided function with the given arguments
+		func(mac_address, max_temp, min_temp);
 
-        // Wait for the specified interval
-        std::this_thread::sleep_for(std::chrono::seconds(intervalInSeconds));
-    }
+		// Wait for the specified interval
+		std::this_thread::sleep_for(std::chrono::seconds(intervalInSeconds));
+	}
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,66 +5,31 @@
 
 
 int main(int argc, char *argv[]) {
-    if (argc < 6) {
-        std::cerr << "Usage: " << argv[0] << " -m <mac_address> <intervalInSeconds> <max_temp> <min_temp>" << std::endl;
-        return -1;
-    }
+	if (argc < 6) {
+		std::cerr << "Usage: " << argv[0] << " -m <mac_address> <intervalInSeconds> <max_temp> <min_temp>" << std::endl;
+		return -1;
+	}
 
-    std::string mac_address = argv[2];
-    int max_temp = std::stoi(argv[3]);
-    int  min_temp = std::stoi(argv[4]);
-    int intervalInSeconds = std::stoi(argv[5]);
-    signal(SIGTSTP, SIG_IGN); // Ignore the SIGTSTP signal
+	std::string mac_address = argv[2];
+	int max_temp = std::stoi(argv[3]);
+	int  min_temp = std::stoi(argv[4]);
+	int intervalInSeconds = std::stoi(argv[5]);
+	signal(SIGTSTP, SIG_IGN); // Ignore the SIGTSTP signal
 
-    if (gpioInitialise() < 0) {
-        std::cerr << "Failed to initialize pigpio" << std::endl;
-        return -1;
-    }
+	if (gpioInitialise() < 0) {
+		std::cerr << "Failed to initialize pigpio" << std::endl;
+		return -1;
+	}
 
-    setInterval([&](const std::string& mac_address, float max_temp, float min_temp) {
-        std::string data = get_data(mac_address);
-        if (data == "F") {
-            std::cerr << "Trying To Connect To The Sensor..." << std::endl;
-            std::cout << std::endl;
-            return;
-        }
-
-        SensorData values(data);
-
-        std::cout << "Value: " << data;
-        std::cout << "Voltage: " << values.getVoltage() << "mV" << std::endl;
-        std::cout << "Humidity: " << values.getHumidity() << "%" << std::endl;
-        std::cout << "Temperature: " << values.getTemperature() << " degrees Celsius" << std::endl;
-        std::cout << std::endl;
-
-        if (max_temp > values.getTemperature() && values.getTemperature() > min_temp) {
-	    gpioWrite(25, 0);
-            gpioWrite(17, 0);
-            gpioWrite(18, 0);
-            gpioWrite(4, 1);
-        }
-
-        else if(max_temp< values.getTemperature()) {
-	    
-	    gpioWrite(25, 1);
-            gpioWrite(4, 0);
-            gpioWrite(18, 0);
-            gpioWrite(17, 1);
-        }
-	
-	else {
-	    
-	    gpioWrite(25, 1);
-	    gpioWrite(4, 0);
-            gpioWrite(17, 0);
-            gpioWrite(18, 1);
-        }
-    }, mac_address, max_temp, min_temp, intervalInSeconds);
+	setInterval([&](const std::string& mac_address, float max_temp, float min_temp) {
+			signal(SIGTSTP, handleSignal);
+			getInfo(mac_address, max_temp, min_temp);
+	}, mac_address, max_temp, min_temp, intervalInSeconds);
 
 
-    // Cleanup pigpio
-    gpioTerminate();
+	// Cleanup pigpio
+	gpioTerminate();
 
-    return 0;
+	return 0;
 }
 
